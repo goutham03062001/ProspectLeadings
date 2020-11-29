51c6cd11c62dcbb590cfba93e89ed5a6cd0d627e
goutham03062001 <66135872+goutham03062001@users.noreply.github.com>
2020-11-29 18:18:05 +0530

    Prospectleads
    Prospect Leading is one of the email marketing website

---
+++ index1.js
@@ -0,0 +1,9 @@
+function openNav() {
+	
+  document.getElementById("mySidepanel").style.width = "230px";
+   document.getElementById("mySidepanel").style.height = "1000px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
